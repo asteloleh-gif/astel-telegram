bc78f9bb5fdacdd30a1da1ed3e4422e5c13ac8b5
asteloleh-gif <astel.oleh@gmail.com>
2026-09-23 13:22:09 -0500

    Test managed bot creation deep link

--- tests/managedCrewManager.test.js
+++ tests/managedCrewManager.test.js
@@ -74,7 +74,8 @@ test("/crewsetup prompts for the first missing managed bot", async () => {
   assert.equal(telegramCalls.length, 2);
   const prompt = telegramCalls[1];
   assert.match(prompt.text, /Tommy the Googler/);
-  assert.equal(prompt.replyMarkup.keyboard[0][0].request_managed_bot.suggested_username, "astel_tommy_bot");
+  assert.match(prompt.replyMarkup.inline_keyboard[0][0].url, /t\.me\/newbot\/astelcore_bot\/astel_tommy_bot/);
+  assert.match(prompt.replyMarkup.inline_keyboard[0][0].url, /name=Tommy%20the%20Googler/);
 });
 
 test("managed_bot update binds the pending agent and configures profile", async () => {
